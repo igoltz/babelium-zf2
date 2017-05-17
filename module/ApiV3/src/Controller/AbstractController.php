79900a972607c8977f57ce409e07738d56d823c8
ddniel16 <ddniel16@gmail.com>
2017-05-17 11:26:18 +0200

    Exercises

--- module/ApiV3/src/Controller/AbstractController.php
+++ module/ApiV3/src/Controller/AbstractController.php
@@ -3,7 +3,7 @@
 namespace ApiV3\Controller;
 
 use Zend\Mvc\Controller\AbstractRestfulController;
-
+use JMS\Serializer\SerializationContext;
 use Zend\View\Model\JsonModel;
 
 class AbstractController
@@ -77,6 +77,57 @@ class AbstractController
 
     }
 
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    protected function getEntityManager()
+    {
+        return $this->getService('Doctrine\ORM\EntityManager');
+    }
+
+    protected function getDoctrineRepository($entityName)
+    {
+        return $this->getEntityManager()->getRepository($entityName);
+    }
+
+    /**
+     * @param array|string $data
+     */
+    protected function jsonResponse($data, $groups = array())
+    {
+
+        $json = new JsonModel();
+        $json->setVariables($this->entityToArray($data, $groups));
+        $this->response->setStatusCode(200);
+
+        return $json;
+
+    }
+
+    protected function entityToArray($entity, $groups = array())
+    {
+
+        $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+
+        if (empty($groups)) {
+            $context = SerializationContext::create();
+        } else {
+            $context = SerializationContext::create()->setGroups($groups);
+        }
+
+        $jsonContent = $serializer->serialize(
+            $entity,
+            'json',
+            $context
+        );
+
+        return json_decode($jsonContent, true);
+
+    }
+
+    /**
+     * @param string $service
+     */
     public function getService($service)
     {
 
@@ -89,6 +140,20 @@ class AbstractController
         }
 
         return false;
+
+    }
+
+    public function getBaseUrl()
+    {
+
+        $url = sprintf(
+            '%s://%s%s',
+            $this->request->getServer()->get('REQUEST_SCHEME'),
+            $this->request->getServer()->get('HTTP_HOST'),
+            $this->request->getBasePath()
+        );
+
+        return $url;
 
     }
 
--- module/ApiV3/src/Controller/ExercisesController.php
+++ module/ApiV3/src/Controller/ExercisesController.php
@@ -2,9 +2,6 @@
 
 namespace ApiV3\Controller;
 
-use Zend\View\Model\JsonModel;
-use Zend\Http\Response;
-
 class ExercisesController
     extends AbstractController
 {
@@ -12,32 +9,123 @@ class ExercisesController
     public function getList()
     {
 
-        $exercises = $this->getService('ExercisesService')->getExercises();
+        $where = array(
+            'status' => 1,
+            'visible' => 1,
+            'ismodel' => 0
+        );
+        $orderBy = array('title' => 'asc');
+
+        /**
+         * @var \Doctrine\ORM\EntityRepository $exercisesRepository
+         */
+        $exercisesRepository = $this->getDoctrineRepository('\ApiV3\Entity\Exercise');
+        $exercises = $exercisesRepository->findBy($where, $orderBy);
+
         if (empty($exercises)) {
             return $this->_notFound();
         }
 
-        $json = new JsonModel();
-        $json->setVariables($exercises);
-        $this->response->setStatusCode(200);
-
-        return $json;
+        return $this->jsonResponse($exercises, array('exercise-list'));
 
     }
 
     public function get($id)
     {
 
-        $exercise = $this->getService('ExercisesService')->getExercise($id);
+        $where = array(
+            'status' => 1,
+            'visible' => 1,
+            'ismodel' => 0,
+            'id' => $id
+        );
+
+        $exercisesRepository = $this->getDoctrineRepository('\ApiV3\Entity\Exercise');
+        $exercise = $exercisesRepository->findOneBy($where);
+
+        /**
+         * @var \ApiV3\Entity\Exercise $exercise
+         */
         if (empty($exercise)) {
             return $this->_notFound();
         }
 
-        $json = new JsonModel();
-        $json->setVariables($exercise);
-        $this->response->setStatusCode(200);
+        $whereMedia = array(
+            'instanceid' => $exercise->getId(),
+            'component' => 'exercise',
+        );
 
-        return $json;
+        $mediaRepository = $this->getDoctrineRepository('\ApiV3\Entity\Media');
+        $media = $mediaRepository->findOneBy($whereMedia);
+
+        /**
+         * @var \ApiV3\Entity\Media $media
+         */
+        if (empty($media)) {
+            return $this->_notFound();
+        }
+
+        $whereMediaRendition = array(
+            'fkMedia' => $media->getId(),
+        );
+
+        $mediaRenditionRepository = $this->getDoctrineRepository('\ApiV3\Entity\MediaRendition');
+        $mediaRendition = $mediaRenditionRepository->findOneBy($whereMediaRendition);
+
+        /**
+         * @var \ApiV3\Entity\MediaRendition $mediaRendition
+         */
+        if (empty($mediaRendition)) {
+            return $this->_notFound();
+        }
+
+        $whereSubtitle = array(
+            'fkMedia' => $media->getId(),
+        );
+
+        $subtitleRepository = $this->getDoctrineRepository('\ApiV3\Entity\Subtitle');
+        $subtitle = $subtitleRepository->findOneBy($whereSubtitle);
+
+        /**
+         * @var \ApiV3\Entity\Subtitle $subtitle
+         */
+        if (empty($subtitle)) {
+            return $this->_notFound();
+        }
+
+        $thumbnailUrl = sprintf(
+            '%s/thumbnail/%s/%02d.jpg',
+            $this->getBaseUrl(),
+            $media->getMediacode(),
+            $media->getDefaultthumbnail()
+        );
+
+        $mediaUrl = sprintf(
+            '%s/media/%s',
+            $this->getBaseUrl(),
+            $mediaRendition->getFilename()
+        );
+
+        $mediaResult = array(
+            'id' => $media->getId(),
+            'filename' => $mediaRendition->getFilename(),
+            'subtitleId' => $subtitle->getId(),
+            'thumbnail' => $thumbnailUrl,
+            'mediaUrl' => $mediaUrl
+        );
+
+        $result = array(
+            'id' => $exercise->getId(),
+            'title' => $exercise->getTitle(),
+            'description' => $exercise->getDescription(),
+            'language' => $exercise->getLanguage(),
+            'difficulty' => $exercise->getDifficulty(),
+            'licence' => $exercise->getLicence(),
+            'username' => $exercise->getFkUser()->getUsername(),
+            'media' => $mediaResult
+        );
+
+        return $this->jsonResponse($result);
 
     }
 
