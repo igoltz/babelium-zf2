00303e6a5a92497af335395cfe6a6ca44534f255
irontec <irontec@babelium-dev.irontec.com>
2017-06-05 12:30:50 +0200

    response

--- module/ApiV3/src/Controller/ResponseController.php
+++ module/ApiV3/src/Controller/ResponseController.php
@@ -47,14 +47,64 @@ class ResponseController
     public function create($data)
     {
 
-        var_dump($this->params()->fromPost());
-        var_dump($this->params()->fromPost('name', 12));
+        $studentId = $this->params()->fromPost('studentId', false);
+        $exerciseId = $this->params()->fromPost('exerciseId', false);
+        $subtitleId = $this->params()->fromPost('subtitleId', false);
+        $characterName = $this->params()->fromPost('characterName', false);
+        $responsehash = $this->params()->fromPost('responsehash', false);
+        $audio = $this->params()->fromPost('audio', false);
+
+        $exerciseRepository = $this->getDoctrineRepository('\ApiV3\Entity\Exercise');
+        $exercise = $exerciseRepository->find($exerciseId);
+
+        $subtitleRepository = $this->getDoctrineRepository('\ApiV3\Entity\Subtitle');
+        $subtitle = $subtitleRepository->find($subtitleId);
 
         $consumer = $this->getService('ConsumerService')
             ->getConsumerByHeaders($this->request->getHeaders());
 
-        var_dump($consumer);
-        die();
+        $response = new \ApiV3\Entity\Response();
+
+        $response->setIsPrivate(1);
+        $response->setThumbnailUri('nothumb.png');
+        $response->setRatingAmount(0);
+        $response->setSource('Red5');
+        $response->setDuration(0);
+        $response->setFileIdentifier(uniqid('moodle-'));
+
+        $now = new \DateTime();
+
+        $response->setAddingDate($now);
+        $response->setPriorityDate($now);
+
+        $response->setCharacterName($characterName);
+
+        $response->setFkExercise($exercise);
+        $response->setFkSubtitle($subtitle);
+
+        $response->setFkUserId($consumer['fk_user_id']);
+
+        $em = $this->getEntityManager();
+        $em->persist($response);
+        $em->flush();
+
+        $pk = $response->getId();
+
+        $generatePath = $this->getService('GeneratePath');
+
+        $pathMediaPk = $generatePath->generate(
+            STORAGE_PATH . '/response',
+            $pk,
+            false
+        );
+        mkdir($pathMediaPk, 0777, true);
+
+        file_put_contents(
+            $pathMediaPk . '/' . $pk . '.wav',
+            base64_decode($audio)
+        );
+
+        return $this->jsonResponse($response);
 
     }
 
